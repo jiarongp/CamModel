9f8ca9d44bc13222d76a150293892b36e8fe4c17
JoiDvsion <garypan4@gmail.com>
2020-01-09 19:24:46 +0100

    fix Pool

--- params.py
+++ params.py
@@ -13,22 +13,3 @@ test_db_path = os.path.join(patches_root,'test.npy')
 patches_db_path = os.path.join(patches_root,'patches.npy')
 
 split_path = 'split.npy'
-
-#
-# gpu_id = 0
-# seed = 197
-#
-#
-# caffe_root = 'caffe'
-# def caffe_txt_path_generator(set_label):
-#     return os.path.join(caffe_root,set_label + '.txt')
-# def caffe_lmdb_path_generator(set_label):
-#     return os.path.join(caffe_root,set_label + '_db')
-# caffe_mean_path = os.path.join(caffe_root,'mean.binaryproto')
-# caffe_snapshot_folder = 'snapshot'
-# caffe_state_file = 'state.npy'
-# caffe_best_model_path = os.path.join(caffe_root,'best.caffemodel')
-#
-# scores_path = 'scores.npy'
-# ppi_pdf_path = 'ppi.pdf'
-
