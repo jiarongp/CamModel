f629b6ac1b079b03f3332278c383d4e355474bdd
JoiDvision <garypan4@gmail.com>
2020-02-14 10:48:34 +0100

    instance test

--- params.py
+++ params.py
@@ -14,10 +14,15 @@ patches_db_path = os.path.join(patches_root,'patches.npy')
 
 split_path = 'split.npy'
 
-robust_root = 'robust'
-robust_db_path = os.path.join(patches_root,'patches.npy')
+unseen_root = 'robust'
 
 compr_root = 'compr'
 compr_img = os.path.join(compr_root,'dresden')
 compr_patches = os.path.join(compr_root,'patches')
+
+ins_root = 'instance'
+ins_csv = os.path.join(ins_root, 'instance.csv')
+ins_patches = os.path.join(ins_root,'patches')
+ins_patches_db = os.path.join(ins_patches,'patches.npy')
+ins_test = os.path.join(ins_root,'test')
 
